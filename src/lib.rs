f428471d494a33b57cfee1aaae75c428e0a48a45
Vo Le Tung <15053897+letung3105@users.noreply.github.com>
2021-06-21 00:09:42 +0700

    function

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@
 
 use std::{
     collections::{HashMap, HashSet},
-    fmt,
     hash::Hash,
 };
 
@@ -18,76 +17,65 @@ pub enum PollResult<'a, T> {
     Winner(&'a T),
 }
 
-/// Structure for holding votes that are used to determine that best option
-pub struct InstantRunoffVotingPoll<'a, T, VoteIter, OptionIter>
-where
-    T: 'a,
-    &'a VoteIter: IntoIterator<Item = &'a OptionIter>,
-    &'a OptionIter: 'a + IntoIterator<Item = &'a T>,
-{
-    votes: &'a VoteIter,
-}
-
-impl<'a, T, VoteIter, OptionIter> InstantRunoffVotingPoll<'a, T, VoteIter, OptionIter>
+/// Determine the best option(s) using the instant-runoff voting system. This function does not
+/// guarantee the winner to be the one receives the majority votes.
+fn run_instant_runoff_voting<'a, T>(votes: &'a [&'a [T]]) -> PollResult<'a, T>
 where
     T: 'a + Eq + Hash,
-    &'a VoteIter: IntoIterator<Item = &'a OptionIter>,
-    &'a OptionIter: 'a + IntoIterator<Item = &'a T>,
+    // &'a VoteIter: IntoIterator<Item = &'a OptionIter>,
+    // &'a OptionIter: 'a + IntoIterator<Item = &'a T>,
 {
-    /// Get the poll result
-    pub fn result(&self) -> PollResult<T> {
-        let mut eliminated_options: HashSet<&T> = HashSet::new();
-        loop {
-            // Count votes
-            let mut votes_count: HashMap<&T, u32> = HashMap::new();
-            for vote in self.votes {
-                for opt in vote {
-                    if !eliminated_options.contains(opt) {
-                        let count = votes_count.entry(opt).or_insert(0);
-                        *count += 1;
-                        break;
-                    }
+    let mut eliminated_options: HashSet<&T> = HashSet::new();
+    loop {
+        // Count votes
+        let mut votes_count: HashMap<&T, u32> = HashMap::new();
+        for &vote in votes {
+            for opt in vote {
+                if !eliminated_options.contains(opt) {
+                    let count = votes_count.entry(opt).or_insert(0);
+                    *count += 1;
+                    break;
                 }
             }
-            // There is no vote
-            if votes_count.is_empty() {
-                break PollResult::NoWinner;
-            }
+        }
+        // There is no vote
+        if votes_count.is_empty() {
+            break PollResult::NoWinner;
+        }
 
-            // Get options with most number of votes and options with least number of votes
-            let mut max_count = u32::MIN;
-            let mut min_count = u32::MAX;
-            let mut best_options = Vec::new();
-            let mut worst_options = Vec::new();
-            for (&k, &v) in votes_count.iter() {
-                if v > max_count {
-                    best_options.clear();
-                    max_count = v;
-                }
-                if v >= max_count {
-                    best_options.push(k);
-                }
-                if v < min_count {
-                    worst_options.clear();
-                    min_count = v;
-                }
-                if v <= min_count {
-                    worst_options.push(k);
-                }
+        // Get options with most number of votes and options with least number of votes
+        let mut max_count = u32::MIN;
+        let mut min_count = u32::MAX;
+        let mut best_options = Vec::new();
+        let mut worst_options = Vec::new();
+        for (&k, &v) in votes_count.iter() {
+            if v > max_count {
+                best_options.clear();
+                max_count = v;
             }
+            if v >= max_count {
+                best_options.push(k);
+            }
+            if v < min_count {
+                worst_options.clear();
+                min_count = v;
+            }
+            if v <= min_count {
+                worst_options.push(k);
+            }
+        }
 
-            // Only one option received the majority of votes
-            if best_options.len() == 1 {
-                break PollResult::Winner(best_options.first().unwrap());
-            }
-            // Tied when votes are evenly distributed
-            if max_count == min_count {
-                break PollResult::Tied(best_options);
-            }
+        // Only one option received the majority of votes
+        if best_options.len() == 1 {
+            break PollResult::Winner(best_options.first().unwrap());
+        }
+        // Tied when votes are evenly distributed
+        if max_count == min_count {
+            break PollResult::Tied(best_options);
+        }
 
-            for opt in worst_options {
-                eliminated_options.insert(opt);
-            }
+        for opt in worst_options {
+            eliminated_options.insert(opt);
         }
     }
 }
@@ -103,10 +91,15 @@ mod tests {
         let vote_c = vec!["bill", "sue", "bob"];
         let vote_d = vec!["bob", "bill", "sue"];
         let vote_e = vec!["sue", "bob", "bill"];
-        let votes = vec![vote_a, vote_b, vote_c, vote_d, vote_e];
 
-        let poll = InstantRunoffVotingPoll { votes: &votes };
-        match poll.result() {
+        let votes = vec![
+            vote_a.as_slice(),
+            vote_b.as_slice(),
+            vote_c.as_slice(),
+            vote_d.as_slice(),
+            vote_e.as_slice(),
+        ];
+        match run_instant_runoff_voting(&votes) {
             PollResult::NoWinner => unreachable!(),
             PollResult::Tied(_) => unreachable!(),
             PollResult::Winner(winner) => assert_eq!(winner, &"sue"),
@@ -117,10 +110,9 @@ mod tests {
     fn irv_tied_01() {
         let vote_a = vec!["bob"];
         let vote_b = vec!["sue"];
-        let votes = vec![vote_a, vote_b];
 
-        let poll = InstantRunoffVotingPoll { votes: &votes };
-        match poll.result() {
+        let votes = vec![vote_a.as_slice(), vote_b.as_slice()];
+        match run_instant_runoff_voting(&votes) {
             PollResult::NoWinner => unreachable!(),
             PollResult::Winner(_) => unreachable!(),
             PollResult::Tied(options) => {
@@ -134,10 +126,9 @@ mod tests {
     fn irv_tied_02() {
         let vote_a = vec!["bob", "sue"];
         let vote_b = vec!["sue", "bob"];
-        let votes = vec![vote_a, vote_b];
 
-        let poll = InstantRunoffVotingPoll { votes: &votes };
-        match poll.result() {
+        let votes = vec![vote_a.as_slice(), vote_b.as_slice()];
+        match run_instant_runoff_voting(&votes) {
             PollResult::NoWinner => unreachable!(),
             PollResult::Winner(_) => unreachable!(),
             PollResult::Tied(options) => {
@@ -149,9 +140,8 @@ mod tests {
 
     #[test]
     fn irv_no_vote() {
-        let votes: Vec<Vec<&str>> = vec![];
-        let poll = InstantRunoffVotingPoll { votes: &votes };
-        match poll.result() {
+        let votes: Vec<&[&str]> = vec![];
+        match run_instant_runoff_voting(&votes) {
             PollResult::Tied(_) => unreachable!(),
             PollResult::Winner(_) => unreachable!(),
             PollResult::NoWinner => {}
